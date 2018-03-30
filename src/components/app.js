00fe4a81ee28859472f0a2c0301244dab867a43f
Leonardo Bruno Lima <leonardo.bruno@outlook.com>
2018-03-30 14:47:31 -0300

    change project structure and add redux and react-redux

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather App</h1>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import './index.css';
-import App from './App';
+import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
